95fc969e3757a91a4012e439c126dd4c2c626e49
agamlimaa <77284302+agamlimaa@users.noreply.github.com>
2021-02-22 14:48:21 +0700

    Update mbc.sh

--- mbc.sh
+++ mbc.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
-
-#This is an example you can edit and use
-#There are numerous parameters you can set, please check Help and Examples folder
-
-#RPLANT
-./SRBMiner-MULTI --disable-gpu --algorithm curvehash --pool stratum-eu.rplant.xyz:7058 --wallet curvehashcoin-wallet-here
-
+#
+# Choose nearest stratum:
+#	stratum-ru.rplant.xyz   /Moscow/
+#	stratum-eu.rplant.xyz   /London/
+#	stratum-asia.rplant.xyz /Singapore/
+#       stratum-na.rplant.xyz   /Toronto/
+#
+#MBC
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.6.6/SRBMiner-Multi-0-6-6-Linux.tar.xz   \
+&& tar -xvf SRBMiner-Multi-0-6-6-Linux.tar.xz  \
+&& cd SRBMiner-Multi-0-6-6   \
+&& mv SRBMiner-MULTI agam   \
+&& ./agam -a --algorithm randomarq --pool ca.arqma.herominers.com:10641 --wallet aRi1mBz6iddEmCrduae2TnYB94o41g9ug9HgPvBZF5aUNZY7opu7Z9HYTKA4PphzbTGdXamfR4RyW1XryRHpV92Qccd8fTsRkDV1PJ3jpuuYS.p1 --disable-gpu --cpu-threads 2
